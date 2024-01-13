bedc6922a13fe035b584fca903a3d479640b4f78
GustawB <93836926+GustawB@users.noreply.github.com>
2024-01-13 13:51:49 +0100

    mimporun now closes all pipes.

--- mimpirun.c
+++ mimpirun.c
@@ -44,20 +44,16 @@ int main(int argc, char** argv) {
     // Create 3n pipes for the group communication.
     for (int i = 0; i < nr_of_copies; ++i) {
         for (int j = 0; j < 3; ++j) {
-            int channel_dscba[2];
-            int channel_dscbr[2];
-            int channel_dscrd[2];
-            ASSERT_SYS_OK(channel(channel_dscba));
-            ASSERT_SYS_OK(channel(channel_dscbr));
-            ASSERT_SYS_OK(channel(channel_dscrd));
+            int channel_dsc[2];
+            ASSERT_SYS_OK(channel(channel_dsc));
             // Move read descriptor to the index = 532+id+j.
-            ASSERT_SYS_OK(dup2(channel_dscba[0], 532 + j + i * 3));
+            ASSERT_SYS_OK(dup2(channel_dsc[0], 532 + j + i * 3));
             // Close the old read descriptor.
-            ASSERT_SYS_OK(close(channel_dscba[0]));
+            ASSERT_SYS_OK(close(channel_dsc[0]));
             // Move write descriptor to the index = 580+id+j.
-            ASSERT_SYS_OK(dup2(channel_dscba[1], 580 + j + i * 3));
+            ASSERT_SYS_OK(dup2(channel_dsc[1], 580 + j + i * 3));
             // Close the old write descriptor.
-            ASSERT_SYS_OK(close(channel_dscba[1]));
+            ASSERT_SYS_OK(close(channel_dsc[1]));
         }
     }
 
